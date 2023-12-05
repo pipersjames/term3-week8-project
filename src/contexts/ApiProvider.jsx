381ac1561dfc42b2454797007c9f1da4a140069d
pipersjames <pipersjames@gmail.com>
2023-12-05 18:45:39 +1000

    initialized API provider

---
+++ src/contexts/ApiProvider.jsx
@@ -0,0 +1,14 @@
+
+// https://pokeapi.co/api/v2/
+
+import { createContext } from "react";
+
+export const ApiContext = createContext("https://pokeapi.co/api/v2/")
+
+export function ApiProvider(props) {
+    return (
+        <ApiContext.Provider value = "https://pokeapi.co/api/v2/">
+            {props.children}
+        </ApiContext.Provider>
+    )
+}
